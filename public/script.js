e9f26d73ff10c21cff6f2201bd7a3cb4983da1b4
nithin <bhandary0010@gmail.com>
2023-01-19 09:18:08 +0000

    change1

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 const quizData = [
     {
-        question: "Which language runs in a web browser?",
+        question: "Which is the hh language runs in a web browser?",
         a: "Java",
         b: "C",
         c: "Python",
